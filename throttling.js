68968365d1fabfdff8b7dd6be62d2695faeb2b04
dalvifaraz <dalvifaraz@gmail.com>
2022-05-10 00:31:30 +0530

    throttling

---
+++ throttling.js
@@ -0,0 +1,46 @@
+console.log("Throttling");
+let count = 0;
+
+function load() {
+  console.log("LOADING....", count++);
+}
+
+const throttling = (func, limit) => {
+  let inThrottle;
+  return function () {
+    const args = arguments;
+    const context = this;
+    if (!inThrottle) {
+      func.apply(context, args);
+      inThrottle = true;
+      setTimeout(() => inThrottle = false, limit);
+    }
+  };
+};
+
+const throttle = (func, limit) => {
+  let lastFunc;
+  let lastRan;
+  return function () {
+    const context = this;
+    const args = arguments;
+    if (!lastRan) {
+      func.apply(context, args);
+      lastRan = Date.now();
+    } else {
+      clearTimeout(lastFunc);
+      lastFunc = setTimeout(function () {
+        if ((Date.now() - lastRan) >= limit) {
+          func.apply(context, args);
+          lastRan = Date.now();
+        }
+      }, limit - (Date.now() - lastRan));
+    }
+  };
+};
+
+const optimizeFunction = throttling(load, 3000);
+
+document.querySelector("#clickMe").addEventListener('click', () => {
+  optimizeFunction();
+});
